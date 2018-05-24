d7ed4eb7595e5b4b4a293ce0f3f53177be7f922e
araki <araki@qoncept.co.jp>
2018-05-24 15:24:03 +0900

    Fix: XCTestManifests

--- Tests/DevRandomTests/XCTestManifests.swift
+++ Tests/DevRandomTests/XCTestManifests.swift
@@ -4,6 +4,8 @@ import XCTest
 public func allTests() -> [XCTestCaseEntry] {
     return [
         testCase(DevRandomTests.allTests),
+        testCase(SingleNumberTests.allTests),
+        testCase(DataTests.allTests),
     ]
 }
 #endif
